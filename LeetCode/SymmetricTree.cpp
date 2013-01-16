2716b45e119abea1ecf0bf674c585f7f641ffbf6
Liu xinhao <vidreams@gmail.com>
2013-01-16 01:42:15 -0800

    Create SymmetricTree.cpp

---
+++ LeetCode/SymmetricTree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+   bool isSameTree(TreeNode *p, TreeNode *q) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(p == NULL && q == NULL)
+        {
+            return true;
+
+        }
+        if(p == NULL || q == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            if(p->val != q->val)
+            {
+                return false;
+            }
+            else
+            {
+                return isSameTree(p->left,q->right) && isSameTree(p->right,q->left);
+            }
+        }
+        
+    }
+    bool isSymmetric(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(root == NULL)
+        {
+            return true;
+
+        }
+        if(root->left == NULL && root->right == NULL)
+        {
+            return true;
+        }
+        if(root->left == NULL || root->right == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            TreeNode *left = root->left;
+            TreeNode *right = root->right;
+            return (left->val == right->val) && isSameTree(left->left,right->right) && isSameTree(left->right,right->left);
+           
+           
+                 
+           
+        }
+        
+        
+    }
+};
