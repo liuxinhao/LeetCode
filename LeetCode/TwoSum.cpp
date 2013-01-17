9fad913bc36f437cb4fbd5fcf8a7865ab72d1920
Liu xinhao <vidreams@gmail.com>
2013-01-17 00:50:10 -0800

    Create TwoSum.cpp

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> ans;
+        for(int i = 0; i < numbers.size(); i++)
+        {
+            for(int j = i + 1; j < numbers.size(); j++)
+            {
+                if(numbers[i] + numbers[j] == target)
+                {
+                    ans.push_back(i+1);
+                    ans.push_back(j+1);
+                    return ans;
+                }
+            }
+        }
+    }
+};
