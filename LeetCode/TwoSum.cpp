b7108f00bad5e9c99acdc62288eab6d6ea3f3319
Liu xinhao <vidreams@gmail.com>
2013-01-17 18:29:47 +0900

    Update LeetCode/TwoSum.cpp

--- LeetCode/TwoSum.cpp
+++ LeetCode/TwoSum.cpp
@@ -1,3 +1,4 @@
+///O(n^2)
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
@@ -19,3 +20,49 @@ public:
         }
     }
 };
+
+/// O(nlogn)
+struct Node
+ {
+     int val;
+     int index;
+     Node(){}
+     Node(int v, int idx):val(v), index(idx){}
+ };
+ 
+ bool compare(const Node &lhs, const Node &rhs)
+ {
+     return lhs.val < rhs.val;
+ }
+ 
+ class Solution {
+ public:
+     vector<int> twoSum(vector<int> &numbers, int target) {
+         // Start typing your C/C++ solution below
+         // DO NOT write int main() function
+         vector<Node> a;
+         for(int i = 0; i < numbers.size(); i++)
+             a.push_back(Node(numbers[i], i + 1));
+         sort(a.begin(), a.end(), compare);
+         
+         int i = 0;
+         int j = numbers.size() - 1;
+         while(i < j)
+         {
+             int sum = a[i].val + a[j].val;
+             if (sum == target)
+             {
+                 vector<int> ret;
+                 int minIndex = min(a[i].index, a[j].index);
+                 int maxIndex = max(a[i].index, a[j].index);
+                 ret.push_back(minIndex);
+                 ret.push_back(maxIndex);
+                 return ret;
+             }
+             else if (sum < target)
+                 i++;
+             else
+                 j--;
+         }
+     }
+ };
