cd82c3d203acf6328dc099f35aa319a8e7e7e617
Liu xinhao <vidreams@gmail.com>
2013-04-08 13:30:45 +0900

    Create ValidNumber.cpp

---
+++ LeetCode/ValidNumber.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isNumber(const char *s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+            while (*s == ' ') {
+  	s++;
+	}
+	cout << *s << endl;
+	if (*s == '+' || *s == '-')
+		s++;
+	bool hasNum = false;
+	while (*s >= '0' && *s <= '9') {
+		hasNum = true;
+		s++;
+	}
+	if (*s == '.') {
+		s++;
+		while (*s >= '0' && *s <= '9') {
+			hasNum = true;
+			s++;
+		}
+	}
+	if (!hasNum)
+		return false;
+	if (*s == 'e' || *s == 'E') {
+		s++;
+		if (*s == '+' || *s == '-')
+			s++;
+
+		hasNum = false;
+		while (*s >= '0' && *s <= '9') {
+			hasNum = true;
+			s++;
+		}
+
+		if (!hasNum)
+			return false;
+	}
+
+	while(*s == ' ') s++;
+
+
+	return *s == '\0';
+        
+    }
+};
