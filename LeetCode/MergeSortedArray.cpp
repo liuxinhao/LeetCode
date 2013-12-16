88c56aa23c95f9d8d474ac2555e2ecf4294ec78e
Liu xinhao <vidreams@gmail.com>
2013-12-16 17:41:53 +0900

    Create MergeSortedArray.cpp

---
+++ LeetCode/MergeSortedArray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int k = m + n - 1, i = m - 1 , j = n - 1;
+        
+        while(i >= 0 && j >= 0)
+        {
+            if(A[i] > B[j]){
+                A[k--] = A[i];
+                i--;
+                
+            }
+            else {
+                A[k--] = B[j];
+                j--;
+            }
+        }
+        while(i >= 0)
+        {
+            A[k--] = A[i--];
+        }
+        while(j >= 0)
+        {
+            A[k--] = B[j--];
+        }
+        
+    }
+};
